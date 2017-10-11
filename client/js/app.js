b21b25235287b5e638e26fcdd25637fd33b67723
Eric W. Greene <eric@ericwgreene.com>
2017-10-11 08:28:43 -0400

    fetch demos

--- client/js/app.js
+++ client/js/app.js
@@ -3,4 +3,24 @@ import * as ReactDOM from 'react-dom';
 
 import { HelloWorld } from './components/hello-world';
 
+// {"query":"query {\n\tbooks {\n    id\n    title\n  }\n}\n","variables":null}
+
+const myFirstGraphQLQuery = {
+  query: "query { books { id title } }",
+  variables: null
+};
+
+fetch('http://localhost:3000/graphql', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify(myFirstGraphQLQuery),
+})
+  .then(res => res.json())
+  .then(results => console.log(results));
+
+Instructions
+
+Create a fetch request to download some car data from the graphql server and
+display it in the console.
+
 ReactDOM.render(<HelloWorld />, document.querySelector('main'));
